db731d05e13faa0a6018a645828f2d9993f9a8d4
Bruno <brunooliveiradsv@gmail.com>
2025-02-15 07:24:07 -0300

    Ok

--- script.js
+++ script.js
@@ -13,3 +13,36 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+
+const carousel = document.querySelector('.carousel');
+const cards = document.querySelectorAll('.card');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+
+let currentPosition = 0; // Posição atual do carrossel
+
+nextButton.addEventListener('click', () => {
+  // Verifica se está no último card
+  if (currentPosition === cards.length - 1) {
+    currentPosition = 0; // Volta para o primeiro card
+  } else {
+    currentPosition++; // Avança para o próximo card
+  }
+  updateCarousel();
+});
+
+prevButton.addEventListener('click', () => {
+  // Verifica se está no primeiro card
+  if (currentPosition === 0) {
+    currentPosition = cards.length - 1; // Volta para o último card
+  } else {
+    currentPosition--; // Retrocede para o card anterior
+  }
+  updateCarousel();
+});
+
+function updateCarousel() {
+  carousel.style.transform = `translateX(-${currentPosition * 100}%)`;
+}
